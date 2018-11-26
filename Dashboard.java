0dd43c70f58da2007aa1085fb47c7678ba962d6c
dcstapleton <44724493+dcstapleton@users.noreply.github.com>
2018-11-26 18:32:35 -0500

    Add files via upload

--- Dashboard.java
+++ Dashboard.java
@@ -3,16 +3,32 @@ package com.hfad.tictactoe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Dashboard extends Activity
 {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainMenu.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_dashboard);
+        new Timer().schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                startActivity(new Intent(getApplicationContext(), MainMenu.class));
+            }
+        }, 5000);
+
+
     }
+
+
 }
