035b0853ec3af9d71dffa0f066f721610477cbf3
Sky <43117461+meliva26@users.noreply.github.com>
2018-12-07 10:02:17 -0500

    Added BoardActivity (there's nothing on there yet)

---
+++ BoardActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
